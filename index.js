6cadd845024c55f2ea0d3aa270070e3bbe95ab29
dan cleary <dancleary@hotmail.com>
2020-04-16 11:46:05 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+am4core.ready(() => {
+    let data = [];
+
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    var categoryAxis = chart.xAxes.push(new am4charts.DateAxis());
+  //  categoryAxis.dataFields.category = "ACCURATE_EPISODE_DATE";
+    categoryAxis.title.text = "Date"
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.title.text = "Total Infections"
+
+    //Define line series
+    series = chart.series.push(new am4charts.LineSeries());
+    series.strokeOpacity = 0;
+    series.name = "Infections";
+    series.dataFields.valueY = "ACCURATE_EPISODE_DATE";
+    series.dataFields.valueX = "ROW_ID";
+
+    chart.data = data;
+})
